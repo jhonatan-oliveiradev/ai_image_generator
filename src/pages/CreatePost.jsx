d12b4ca82a432b6f33bb3670889c989ba7786c05
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-01-21 13:12:13 -0300

    :sparkles: Finalizando publicacao de imagens e mostrando na Home

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -42,7 +42,37 @@ const CreatePost = () => {
 		}
 	};
 
-	const handleSubmit = () => {};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (form.name && form.photo) {
+			setLoading(true);
+
+			try {
+				const response = await fetch("http://localhost:8080/api/v1/post", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(form),
+				});
+
+				const data = await response.json();
+
+				if (data.success) {
+					navigate("/");
+				} else {
+					alert("Something went wrong");
+				}
+			} catch (error) {
+				alert(error);
+			} finally {
+				setLoading(false);
+			}
+		} else {
+			alert("Please enter a and generate an image");
+		}
+	};
 
 	const handleChange = (e) => {
 		setForm({ ...form, [e.target.name]: e.target.value });
