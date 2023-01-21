7f4d311bad4196204d3a7667b30aa504a20f0528
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-01-21 13:50:12 -0300

    :wrench: inserindo url do servidor online

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -19,15 +19,18 @@ const CreatePost = () => {
 		if (form.prompt) {
 			try {
 				setGeneratingImg(true);
-				const response = await fetch("http://localhost:8080/api/v1/dalle", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						prompt: form.prompt,
-					}),
-				});
+				const response = await fetch(
+					"https://dalle-e-backend-3.onrender.com/api/v1/dalle",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({
+							prompt: form.prompt,
+						}),
+					}
+				);
 
 				const data = await response.json();
 
@@ -49,13 +52,16 @@ const CreatePost = () => {
 			setLoading(true);
 
 			try {
-				const response = await fetch("http://localhost:8080/api/v1/post", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify(form),
-				});
+				const response = await fetch(
+					"https://dalle-e-backend-3.onrender.com/api/v1/post",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(form),
+					}
+				);
 
 				const data = await response.json();
 
