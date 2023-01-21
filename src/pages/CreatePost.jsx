21e796c136341af535674b2ea44e39675a8a111c
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-01-20 23:26:47 -0300

    :sparkles: feat: conectando com o backend

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import { preview } from "../assets";
 import { getRandomPrompt } from "../utils";
-import { FormField } from "../components";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
 	const navigate = useNavigate();
@@ -15,11 +15,43 @@ const CreatePost = () => {
 	const [generatingImg, setGeneratingImg] = useState(false);
 	const [loading, setLoading] = useState(false);
 
+	const generateImage = async () => {
+		if (form.prompt) {
+			try {
+				setGeneratingImg(true);
+				const response = await fetch("http://localhost:8080/api/v1/dalle", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						prompt: form.prompt,
+					}),
+				});
+
+				const data = await response.json();
+
+				setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+			} catch (error) {
+				alert(error);
+			} finally {
+				setGeneratingImg(false);
+			}
+		} else {
+			alert("Please enter a prompt");
+		}
+	};
+
 	const handleSubmit = () => {};
 
-	const handleChange = (e) => {};
+	const handleChange = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
 
-	const handleSurpriseMe = () => {};
+	const handleSurpriseMe = () => {
+		const randomPrompt = getRandomPrompt(form.prompt);
+		setForm({ ...form, prompt: randomPrompt });
+	};
 
 	return (
 		<section className="max-w-7xl mx-auto">
@@ -46,12 +78,57 @@ const CreatePost = () => {
 						labelName="Prompt"
 						type="text"
 						name="prompt"
-						placeholder="A cat is sitting on a table"
+						placeholder="A 3D render of a rainbow colored hot air balloon flying above a reflective lake"
 						value={form.prompt}
 						handleChange={handleChange}
 						isSurpriseMe
 						handleSurpriseMe={handleSurpriseMe}
 					/>
+
+					<div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+						{form.photo ? (
+							<img
+								src={form.photo}
+								alt={form.prompt}
+								className="w-full h-full object-contain"
+							/>
+						) : (
+							<img
+								src={preview}
+								alt="preview"
+								className="w-9/12 h-9/12 object-contain opacity-40"
+							/>
+						)}
+
+						{generatingImg && (
+							<div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0, 0, 0, 0.5)] rounded-lg">
+								<Loader />
+							</div>
+						)}
+					</div>
+				</div>
+
+				<div className="mt-4 flex gap-5">
+					<button
+						type="button"
+						onClick={generateImage}
+						className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+					>
+						{generatingImg ? "Generating..." : "Generate"}
+					</button>
+				</div>
+
+				<div className="mt-10">
+					<p className="text-[#666e75] mt-2 text-[14px]">
+						Once you have created the image you want, you can share it with
+						others in the community.
+					</p>
+					<button
+						type="submit"
+						className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+					>
+						{loading ? "Sharing..." : "Share with the community"}
+					</button>
 				</div>
 			</form>
 		</section>
